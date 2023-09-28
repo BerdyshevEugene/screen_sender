6a83ba8bbcd24e0eb31682694a6829a4ee444614
BerdyshevEugene <berdyshevevg@mail.ru>
2023-09-28 15:49:12 +0300

    init: start screen_sender

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# screen_sender
-to save screenshots
+# Screen_Sender
+
+## Программа для захвата скриншотов и сохранения в заданной директории
+
+## Структура проекта:
+
+```python
+
+screen_sender
+│
+├── screen_sender
+│   └── main.py - основная часть программы
+│
+├── requirements.txt - зависимости
+└── README.md
+
+```
+
+## Инструкция
+1. создайте и активируйте виртуальное окружение и установите зависимости:
+```python
+py -m venv venv
+.\venv\Scripts\Activate.ps1
+pip install -r requirements.txt
+```
+
+2. скомпилируйте в .exe:
+pyinstaller --noconfirm --noconsole --onefile --name=screen_sender --icon=media/icon_.png main.py
+
+
+## Остальная информация 
+
+ver. 1.0.0
+
+CompanyName: GMG
+
+FileDescription: Screen sender
+
+InternalName: Click_click_screen
+
+LegalCopyright: © GMG. All rights reserved.
+
+OriginalFilename: screen_sender.exe
+
+ProductName: Screen sender
+
+Author: Berdyshev E.A.
+
+Development and support: Berdyshev E.A.
+
+![Python](https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white)
---
+++ screen_sender/main.py
@@ -0,0 +1,158 @@
+import datetime
+import keyboard
+import os
+import time
+import sys
+
+from pathlib import Path
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5 import *
+from PIL import ImageGrab
+
+
+class ScreenshotWorker(QObject):
+    capture_screenshot_signal = pyqtSignal()
+
+    @pyqtSlot()
+    def capture_screenshot(self):
+        self.capture_screenshot_signal.emit()
+
+
+class ScreenshotThread(QThread):
+    screenshot_saved_signal = pyqtSignal(str)
+
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        self.capture_screenshot()
+
+    @pyqtSlot()
+    def capture_screenshot(self):
+        timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        default_file_name = f'screenshot_{timestamp}.jpg'
+
+        time.sleep(0.2)
+
+        options = QFileDialog.Options()
+        options |= QFileDialog.ReadOnly
+
+        name, _ = QFileDialog.getSaveFileName(
+            None, 'Сохранить файл', '', 'Images (*.png *.jpg)', options=options)
+
+        time.sleep(0.2)
+
+        if name:
+            screenshot = ImageGrab.grab()
+            screenshot_path = os.path.splitext(
+                name)[0] + '_' + timestamp + '.jpg'
+            screenshot.save(screenshot_path)
+            self.screenshot_saved_signal.emit(screenshot_path)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setMinimumSize(QSize(500, 200))
+        self.setWindowTitle('Click click screen')
+        central_widget = QWidget(self)
+        self.setCentralWidget(central_widget)
+
+        central_layout = QVBoxLayout(central_widget)
+        central_layout.addWidget(
+            QLabel('1. Чтобы сделать скриншот экрана, нажмите ctrl+alt+s.\n'
+                   '2. Чтобы убрать программу в трей, кликните ниже.', self))
+
+        self.hide_button = QPushButton('Спрячь меня')
+        self.hide_button.setFixedSize(100, 25)
+        central_layout.addWidget(self.hide_button)
+        central_layout.addItem(QSpacerItem(
+            0, 0, QSizePolicy.Expanding, QSizePolicy.Expanding))
+
+        # directory section
+        dir_layout = QHBoxLayout()
+        self.dir_name_edit = QLineEdit()
+        dir_layout.addWidget(QLabel('Путь:'))
+        dir_layout.addWidget(self.dir_name_edit)
+
+        dir_btn = QPushButton('Обзор')
+        dir_btn.clicked.connect(self.open_dir_dialog)
+        dir_btn.setFixedSize(70, 25)
+
+        dir_layout.addWidget(dir_btn)
+        central_layout.addLayout(dir_layout)
+
+        version_label = QLabel('ver. 1.0.0')
+        version_label.setAlignment(QtCore.Qt.AlignRight)
+        central_layout.addWidget(version_label)
+
+        self.show()
+
+        # настроки иконок
+        icon_path = (
+            r'C:\Users\adm03\Desktop\work\programming\click_click_screen\icon\icon_.png')
+        # иконка приложения
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(icon_path),
+                       QtGui.QIcon.Selected, QtGui.QIcon.On)
+        self.setWindowIcon(icon)
+        # иконка трея
+        self.tray_icon = QSystemTrayIcon(self)
+        self.tray_icon.setIcon(
+            QIcon(icon_path))
+
+        show_action = QAction('☐   Открыть', self)
+        hide_action = QAction('▼   Свернуть', self)
+        quit_action = QAction('✕   Закрыть click click screen', self)
+        show_action.triggered.connect(self.show)
+        hide_action.triggered.connect(self.hide)
+        quit_action.triggered.connect(QApplication.instance().quit)
+        # меню трея
+        tray_menu = QMenu()
+        tray_menu.addAction(show_action)
+        tray_menu.addAction(hide_action)
+        tray_menu.addAction(quit_action)
+        self.tray_icon.setContextMenu(tray_menu)
+        self.tray_icon.show()
+
+        self.screenshot_thread = ScreenshotThread()
+        self.screenshot_thread.screenshot_saved_signal.connect(
+            self.screenshot_saved)
+
+        # обработчик изменения состояния кнопки
+        self.hide_button.clicked.connect(self.handle_hide_button_click)
+
+        keyboard.add_hotkey(
+            'ctrl+alt+s', self.capture_and_save_screenshot)
+        self.screenshot_worker = ScreenshotWorker()
+        self.screenshot_worker.capture_screenshot_signal.connect(
+            self.screenshot_thread.capture_screenshot)
+
+    def capture_and_save_screenshot(self):
+        self.screenshot_worker.capture_screenshot()
+
+    def screenshot_saved(self, screenshot_path):
+        print(f'Screenshot saved at {screenshot_path}')
+
+    def handle_hide_button_click(self):
+        if self.isHidden():
+            self.show()
+        else:
+            self.hide()
+
+    def open_dir_dialog(self):
+        dir_name = QFileDialog.getExistingDirectory(self, 'Выберите папку')
+        if dir_name:
+            path = Path(dir_name)
+            self.dir_name_edit.setText(str(path))
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    app.setQuitOnLastWindowClosed(False)
+    mw = MainWindow()
+    mw.hide()
+    sys.exit(app.exec_())
